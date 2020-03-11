597fddd4abda035160eaefa42b19d9f4d7c2e274
Kevin <knphillips01@gmail.com>
2020-03-11 15:59:26 -0500

    finished mvp

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,14 +2,15 @@ import { useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
 export const useDarkMode = (initialValue) => {
-    const [darkModeBoolean, setDarkMode] = useLocalStorage('darkmodesetting', initialValue);
+    const [theme, setTheme] = useLocalStorage('darkmode', initialValue);
 
+    // check value from useLocalStorage if true/false
     useEffect(() => {
-        darkModeBoolean ?
-            document.querySelector("body").classList.add("dark-mode") :
-            document.querySelector("body").classList.remove("dark-mode")
-    },[darkModeBoolean])
-
-    return [darkModeBoolean, setDarkMode];
-
+        if (theme === true) {
+            return document.body.classList.add("dark-mode")
+        } else {
+            return document.body.classList.remove("dark-mode")
+        }
+    }, [theme])
+    return [theme, setTheme]
 }
